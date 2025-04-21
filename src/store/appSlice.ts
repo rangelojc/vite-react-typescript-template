28f76e9d87aa2acc88f7dfb02147078c6985a313
Angelo <ramuzuconcepcion@gmail.com>
2025-04-21 09:18:19 +0800

    refactor: simplify ThemeProvider by using app store for theme management

--- src/store/appSlice.ts
+++ src/store/appSlice.ts
@@ -1,14 +1,21 @@
+import { Theme } from "@/context/ThemeProvider";
 import { persist } from "zustand/middleware";
 
 export interface AppSlice {
   language: string;
   setLanguage: (lang: string) => void;
+
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 }
 
 export const createAppSlice = persist<AppSlice>(
   (set) => ({
-    language: "en",
+    language: "ko",
     setLanguage: (lang) => set({ language: lang }),
+
+    theme: "light",
+    setTheme: (theme) => set({ theme }),
   }),
   {
     name: "app-zustand",
